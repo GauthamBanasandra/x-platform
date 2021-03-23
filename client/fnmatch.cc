6ad33a263310b93e4b05c28e606be0ed84794ef9
Gautham Banasandra <gautham.bangalore@gmail.com>
2021-03-23 21:58:12 +0530

    Add OpenSSL dependency

--- client/fnmatch.cc
+++ client/fnmatch.cc
@@ -24,5 +24,30 @@ int main(int argc, char *argv[]) {
   str = "z.x";
   std::cout << "(" << pattern << ", " << str
             << ") = " << XPlatform::Syscall::FnMatch(pattern, str) << std::endl;
+
+  pattern = "?.z.x";
+  str = "1.z.x";
+  std::cout << "(" << pattern << ", " << str
+            << ") = " << XPlatform::Syscall::FnMatch(pattern, str) << std::endl;
+
+  pattern = "*.doc";
+  str = "some/path/abcd.doc";
+  std::cout << "(" << pattern << ", " << str
+            << ") = " << XPlatform::Syscall::FnMatch(pattern, str) << std::endl;
+
+  pattern = "*.doc";
+  str = R"(some\path\abcd.doc)";
+  std::cout << "(" << pattern << ", " << str
+            << ") = " << XPlatform::Syscall::FnMatch(pattern, str) << std::endl;
+
+  pattern = "x*.doc";
+  str = "y/abcd.doc";
+  std::cout << "(" << pattern << ", " << str
+            << ") = " << XPlatform::Syscall::FnMatch(pattern, str) << std::endl;
+
+  pattern = "x*.doc";
+  str = R"(y\abcd.doc)";
+  std::cout << "(" << pattern << ", " << str
+            << ") = " << XPlatform::Syscall::FnMatch(pattern, str) << std::endl;
   return 0;
 }
